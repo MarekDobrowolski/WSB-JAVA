fe8a26c10149399f974d16b9f151987e253807b4
marek <marekd@gmail.com>
2020-06-24 20:01:55 +0200

    Nowe

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,47 +7,54 @@ public class Main
     {
 
         Human me = new Human();
-        me.animalPet = "Jack";
+        me.firstname = "Jack";
+        me.lastname = "Sparrow";
+        me.animalPet = "John";
         Animal AnimalPet = new Animal("dog");
 
-        AnimalPet.Feed();
-        AnimalPet.Feed();
-        AnimalPet.Feed();
-        AnimalPet.Feed();
-        AnimalPet.Feed();
-        AnimalPet.Feed();
-        AnimalPet.Feed();
-        AnimalPet.Feed();
-        AnimalPet.Feed();
-        AnimalPet.Feed();
+        AnimalPet.feed();
+        AnimalPet.feed();
+        AnimalPet.feed();
+        AnimalPet.feed();
+        AnimalPet.feed();
+        AnimalPet.feed();
+        AnimalPet.feed();
 
-        AnimalPet.TakeForAWalk();
-        AnimalPet.TakeForAWalk();
-        AnimalPet.TakeForAWalk();
-        AnimalPet.TakeForAWalk();
-        AnimalPet.TakeForAWalk();
-        AnimalPet.TakeForAWalk();
-        AnimalPet.TakeForAWalk();
-        AnimalPet.TakeForAWalk();
-        AnimalPet.TakeForAWalk();
-        AnimalPet.TakeForAWalk();
-        AnimalPet.TakeForAWalk();
-        AnimalPet.TakeForAWalk();
-        AnimalPet.TakeForAWalk();
-        AnimalPet.TakeForAWalk();
-        AnimalPet.TakeForAWalk();
-        AnimalPet.TakeForAWalk();
-        AnimalPet.TakeForAWalk();
-        AnimalPet.TakeForAWalk();
-        AnimalPet.TakeForAWalk();
-        AnimalPet.TakeForAWalk();
-        AnimalPet.TakeForAWalk();
-        AnimalPet.TakeForAWalk();
-        AnimalPet.TakeForAWalk();
-        AnimalPet.TakeForAWalk();
-        AnimalPet.TakeForAWalk();
-        AnimalPet.TakeForAWalk();
-        AnimalPet.TakeForAWalk();
+
+        AnimalPet.takeForAWalk();
+        AnimalPet.takeForAWalk();
+        AnimalPet.takeForAWalk();
+        AnimalPet.takeForAWalk();
+        AnimalPet.takeForAWalk();
+        AnimalPet.takeForAWalk();
+        AnimalPet.takeForAWalk();
+        AnimalPet.takeForAWalk();
+        AnimalPet.takeForAWalk();
+        AnimalPet.takeForAWalk();
+        AnimalPet.takeForAWalk();
+        AnimalPet.takeForAWalk();
+        AnimalPet.takeForAWalk();
+        AnimalPet.takeForAWalk();
+        AnimalPet.takeForAWalk();
+        AnimalPet.takeForAWalk();
+        AnimalPet.takeForAWalk();
+        AnimalPet.takeForAWalk();
+        AnimalPet.takeForAWalk();
+        AnimalPet.takeForAWalk();
+        AnimalPet.takeForAWalk();
+        AnimalPet.takeForAWalk();
+        AnimalPet.takeForAWalk();
+        AnimalPet.feed();
+        AnimalPet.feed();
+
+        Car car = new Car();
+        me.car.colour = "black";
+        me.car.model = "Ibiza";
+        me.car.producer = "Seat";
+
+
+
+
 
 
 
