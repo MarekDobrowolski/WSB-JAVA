a04a463060db7d2b31bd5ba40608c5a5dc434139
marek <marekd@gmail.com>
2020-09-17 12:49:15 +0200

    Rozpoczęscie zadań od początku: Zadanie 0

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,71 +2,7 @@ package com.company;
 
 public class Main
 {
-
-    public static void main(String[] args)
-    {
-
-        Human me = new Human();
-        me.firstname = "Jack";
-        me.lastname = "Sparrow";
-        me.animalPet = "John";
-        me.setSalary(2000.00);
-        Animal AnimalPet = new Animal("dog");
-
-        AnimalPet.feed();
-        AnimalPet.feed();
-        AnimalPet.feed();
-        AnimalPet.feed();
-        AnimalPet.feed();
-        AnimalPet.feed();
-        AnimalPet.feed();
-
-
-        AnimalPet.takeForAWalk();
-        AnimalPet.takeForAWalk();
-        AnimalPet.takeForAWalk();
-        AnimalPet.takeForAWalk();
-        AnimalPet.takeForAWalk();
-        AnimalPet.takeForAWalk();
-        AnimalPet.takeForAWalk();
-        AnimalPet.takeForAWalk();
-        AnimalPet.takeForAWalk();
-        AnimalPet.takeForAWalk();
-        AnimalPet.takeForAWalk();
-        AnimalPet.takeForAWalk();
-        AnimalPet.takeForAWalk();
-        AnimalPet.takeForAWalk();
-        AnimalPet.takeForAWalk();
-        AnimalPet.takeForAWalk();
-        AnimalPet.takeForAWalk();
-        AnimalPet.takeForAWalk();
-        AnimalPet.takeForAWalk();
-        AnimalPet.takeForAWalk();
-        AnimalPet.takeForAWalk();
-        AnimalPet.takeForAWalk();
-        AnimalPet.takeForAWalk();
-        AnimalPet.feed();
-        AnimalPet.feed();
-
-        Car car = new Car();
-        me.setCar().colour = "black";
-        me.setCar().model = "Ibiza";
-        me.setCar().producer = "Seat";
-        me.setCar().value = 1900.00;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    public static void main(String[] args) {
 
 
 
