d4a8571c7b757a4b67ea305e875e35067ea036f9
marek <marekd@gmail.com>
2020-09-17 13:53:52 +0200

    Zadanie 3 i 4

--- src/com/company/Human.java
+++ src/com/company/Human.java
@@ -3,5 +3,29 @@ package com.company;
 public class Human {
     Animal animal;
     Car car;
+    private Double salary;
 
+    public Double getSalary() {
+
+        java.util.Date date = new java.util.Date();
+        System.out.println("Your salary is: " + this.salary + "Checked at " + date);
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        if (salary < 0) {
+            System.out.println("Value must be greater than 0");
+        }
+        else {
+
+            System.out.println(" The new payment has been sent to the accountant");
+            System.out.println("The new annex to the contract can be collected from the accountant");
+            System.out.println("ZUS and US already know about the new amount");
+
+            this.salary = salary;
+
+        }
+
+
+    }
 }
