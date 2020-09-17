43cbb3b0a2ab396df982d567ecbd5e62e5b53f4c
marek <marekd@gmail.com>
2020-09-17 13:28:21 +0200

    Zadanie 1

--- src/com/company/Animal.java
+++ src/com/company/Animal.java
@@ -1,4 +1,47 @@
 package com.company;
 
 public class Animal {
+    final String species;
+    private double weight;
+
+    public Animal(String species) {
+        this.species = species;
+
+        if (species == "dog") {
+            this.weight = 10.0;
+        }
+        else if (species == "cat") {
+            this.weight = 7.0;
+        }
+        else if (species == "cow") {
+            this.weight = 50.0;
+        } else {
+            this.weight = 13.0;
+        }
+    }
+
+    public void feed() {
+
+
+        if (this.weight > 0) {
+            this.weight += 1;
+            System.out.println("Weight of animal is: " + this.weight);
+        }
+        if (this.weight <= 0)
+            System.out.println("Sorry, Your animal is dead!");
+
+
+    }
+
+    public void takeForAWalk() {
+
+        if (this.weight > 0){
+            this.weight -= 1;
+            System.out.println("Weight of animal is: " + this.weight);}
+        if (this.weight <= 0)
+            System.out.println("Sorry, Your animal is dead! Don't walk with dead animal");
+
+
+    }
+
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,15 @@
 package com.company;
 
-public class Main
-{
+public class Main {
     public static void main(String[] args) {
 
+        Animal animial = new Animal("cat");
+        animial.feed();
 
+        for (int i = 0; i < 12; i++) {
+            animial.takeForAWalk();
+        }
 
-
+        animial.feed();
     }
 }
